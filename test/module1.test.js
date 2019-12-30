1ab284d226fa392eca113b1b0e56997756603fc0
Liam McLennan <liam.mclennan@gmail.com>
2019-12-31 08:27:39 +1100

    Updated from solution branch

--- test/module1.test.js
+++ test/module1.test.js
@@ -8,14 +8,13 @@ describe("Conway's Game of Life", () => {
         gameoflife.seed,
         "Have you created and exported a `seed` function?"
       );
-    });
 
-    it("Should have a `seed` function that converts arguments to a real array. @seed-returns-array", () => {
       assert(gameoflife.seed &&
         Array.isArray(gameoflife.seed([1, 2], [5, 6])) &&
           gameoflife.seed([1, 2], [5, 6]).length === 2,
         "Have you converted `arguments` to a real array?"
       );
+
     });
   });
 
@@ -25,9 +24,15 @@ describe("Conway's Game of Life", () => {
         gameoflife.same,
         "Have you created and exported a `same` function?"
       );
-    });
 
-    it("Should have a `same` function that tests if two points are the same. @same-function-comparison", () => {
+      var sameNode;
+      esprima.parseModule(source, {}, function(node) {
+        if (node.id && node.id.name === "same") {
+          sameNode = node;
+        }
+      });
+      assert(sameNode.params.length === 2, "Have you created a `same` function with two arguments?");
+
       assert(gameoflife.same && 
         gameoflife.same([1, 2], [1, 2]),
         "Have you created a `same` function that returns true if the two point parameters are the same?"
@@ -36,6 +41,7 @@ describe("Conway's Game of Life", () => {
         !gameoflife.same([1, 2], [5, 2]),
         "Have you created a `same` function that returns false if the two point parameters are not the same?"
       );
+
     });
   });
 
@@ -45,9 +51,7 @@ describe("Conway's Game of Life", () => {
         gameoflife.contains,
         "Have you created and exported a `contains` function?"
       );
-    });
 
-    it("Should have a `contains` function that tests if a cell is alive within the passed game state. @contains-test", () => {
       const boundContains = (gameoflife.contains || (() =>{})).bind([
         [1, 2],
         [3, 4],
@@ -66,14 +70,13 @@ describe("Conway's Game of Life", () => {
         "Have you implemented a check that the passed cell is in the passed game state?"
       );
     });
+
   });
 
   describe("Sum function", () => {
     it("Should have a `sum` function. @sum-function", () => {
       assert(gameoflife.sum, "Have you created and exported a `sum` function?");
-    });
 
-    it("Should have a `sum` function that sums two cells. @sum-addition", () => {
       assert(gameoflife.sum && gameoflife.same &&
         gameoflife.same(gameoflife.sum([1, 2], [5, 7]), [6, 9]),
         "Have you implemented a sum function that adds two cells?"
@@ -82,9 +85,7 @@ describe("Conway's Game of Life", () => {
         gameoflife.same(gameoflife.sum([-1, 2], [5, -7]), [4, -5]),
         "Have you implemented a sum function that handles negative coordinates?"
       );
-    });
 
-    it("Should have a `sum` function that is a single-line arrow function. @sum-arrow-function", () => {
       var sumNode;
       esprima.parseModule(source, {}, function(node) {
         if (node.type === "VariableDeclarator" && node.id.name === "sum") {
@@ -100,6 +101,7 @@ describe("Conway's Game of Life", () => {
         sumNode.init.body.type === "ArrayExpression",
         "Have you implemented an arrow function named `sum`?"
       );
+
     });
   });
 
@@ -108,21 +110,17 @@ describe("Conway's Game of Life", () => {
       assert(
         gameoflife.printCell,
         "Have you created and exported a `printCell` function?");
-    });
 
-    it("Should return ▣ for a living cell. @printCell-living", () => {
       assert(gameoflife.printCell && gameoflife.printCell([1,1], [[1,1], [2,2]]) == "\u25A3", "Have you returned '\u25A3' for living cells?");
-    });
-    
-    it("Should return ▢ for a non-living cell. @printCell-non-living", () => {
       assert(gameoflife.printCell && gameoflife.printCell([1,2], [[1,1], [2,2]]) == "\u25A2", "Have you returned '\u25A2' for non-living cells?");
+
     });
   });
 
   describe("Finding the corners", () => {
     var corners;
     before(()=>{
-      corners = (gameoflife.corners || (()=>{}))([
+      corners = gameoflife.corners([
         [2, 3],
         [2, 1],
         [4, 3],
@@ -136,38 +134,29 @@ describe("Conway's Game of Life", () => {
       assert(
         gameoflife.corners,
         "Have you created and exported a `printCell` function?");
-    });
-
-    it('Should return zeros if there are no living cells. @corners-no-living-cells', () => {
+        
       const zeroCorners = (gameoflife.corners || (() => {}))();
       assert(gameoflife.same && gameoflife.corners && gameoflife.same(zeroCorners.topRight, [0,0]), "Have you ensured that topRight is [0,0] if there are no living cells?");
       assert(gameoflife.same && gameoflife.corners && gameoflife.same(zeroCorners.bottomLeft, [0,0]), "Have you ensured that botomLeft is [0,0] if there are no living cells?");
-    });
 
-    it("Should find a top right. @corners-find-top-right", () => {
       assert(gameoflife.corners && corners.topRight, "");
       assert(gameoflife.corners && Array.isArray(corners.topRight), "");
       assert(gameoflife.corners && corners.topRight.length === 2, "");
-    });
 
-    it("Should find the correct top right. @corners-find-correct-top-right", () => {
       assert(gameoflife.same && gameoflife.corners &&
         gameoflife.same(corners.topRight, [4, 3]),
         "Have you implemented a corners function that returns the correct top right coordinate?"
       );
-    });
 
-    it("Should find a bottom left. @corners-find-bottom-left", () => {
       assert(gameoflife.corners && corners.bottomLeft, "");
       assert(gameoflife.corners && Array.isArray(corners.bottomLeft), "");
       assert(gameoflife.corners && corners.bottomLeft.length === 2, "");
-    });
-
-    it("Should find the correct bottom left. @corners-find-correct-bottom-left", () => {
+      
       assert(gameoflife.same && gameoflife.corners &&
         gameoflife.same(corners.bottomLeft, [1, 1]),
         "Have you implemented a corners function that returns the correct bottom left coordinate?"
       );
+
     });
 
     it("Should have a default parameter. @corners-has-default-parameter", ()=> {
@@ -177,19 +166,19 @@ describe("Conway's Game of Life", () => {
           cornersNode = node;
         }
       });
-      console.log(cornersNode.init.params[0].type == 'AssignmentPattern');
+      //console.log(cornersNode.init.params[0].type == 'AssignmentPattern');
     });
   });
 
-  describe('Calculating the next state', ()=>{
-    var start, next;
-    before(()=>{
-      start = (gameoflife.seed || (()=>{}))([3,2], [2,3],[3,3],[3,4],[4,4]);
-      next = (gameoflife.calculateNext || (()=>{}))(start);
-    });
+  // describe('Calculating the next state', ()=>{
+  //   var start, next;
+  //   before(()=>{
+  //     start = gameoflife.seed([3,2], [2,3],[3,3],[3,4],[4,4]);
+  //     next = gameoflife.calculateNext(start);
+  //   });
 
-    it('should calculate the correct next state. @calculateNext-correct-next-state', ()=>{
-      // todo
-    });
-  });
+  //   it('should calculate the correct next state. @calculateNext-correct-next-state', ()=>{
+  //     // todo
+  //   });
+  // });
 });
